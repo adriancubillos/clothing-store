8cf22d53c7eba8e9e8ea96c40e5bec14cc7aa2cd
Adrian <adriancubillos@gmail.com>
2022-03-04 18:30:03 -0500

    Adding navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Routes, Route, Link, useLocation, useParams } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS page</h1>
+  </div>
+);
+
+const NotFound = () => (
+  <div>
+    <h1>NOT FOUND!</h1>
+    <Link to='/'>Home</Link> |{' '}
+  </div>
+);
+
 function App() {
+  let location = useLocation();
+  console.log(location);
+
+  let params = useParams();
+  console.log(params);
+
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop/hats' element={<HatsPage />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }

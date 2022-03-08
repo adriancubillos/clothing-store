9e2769b987ecc75a3cdd08b8e12d8c7ae80e1288
Adrian <adriancubillos@gmail.com>
2022-03-07 23:41:23 -0500

    form and signIn components

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInAndSignUpPage = () => {
-  return <div>SignInAndSignUpPage</div>;
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
 };
 
 export default SignInAndSignUpPage;

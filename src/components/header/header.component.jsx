9347bb1f32ca676a6f93742157ac16dae491cb5a
Adrian <adriancubillos@gmail.com>
2022-03-07 22:51:27 -0500

    fixing typo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.syles.scss';
+import './header.styles.scss';
 
 const Header = () => (
   <div className='header'>

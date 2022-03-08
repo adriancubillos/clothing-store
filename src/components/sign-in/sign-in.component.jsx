12f6100fa9e92a9a7be6cbe3f50d7fa226b8db1b
Adrian <adriancubillos@gmail.com>
2022-03-08 02:50:22 -0500

    Adding google signin styling and other style fixes

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-Input/form-input.component';
@@ -16,13 +17,19 @@ class SignIn extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+
+    this.setState({
+      email: '',
+      password: '',
+    });
   };
 
   handleChange = (event) => {
     const { value, name } = event.target;
 
-    this.setState({ [name]: value });
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
@@ -30,7 +37,6 @@ class SignIn extends Component {
       <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
-
         <form onSubmit={this.handleSubmit}>
           <FormInput
             name='email'
@@ -48,10 +54,12 @@ class SignIn extends Component {
             label='password'
             required
           />
-          <CustomButton type='submit'> Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );

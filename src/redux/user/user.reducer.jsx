dfbb5355249ad304f239b488ca331fea6573e160
Adrian <adriancubillos@gmail.com>
2022-03-09 18:21:29 -0500

    install and configure Redux

---
+++ src/redux/user/user.reducer.jsx
@@ -0,0 +1,20 @@
+import * as userActions from './user.actions';
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case userActions.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;

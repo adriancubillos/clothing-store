771384a3762b904e4c2da4f52959d09a6e50ca6b
Adrian <adriancubillos@gmail.com>
2022-03-10 15:49:52 -0500

    Replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ onToggleCartHidden, itemCount }) => (
@@ -11,16 +12,9 @@ const CartIcon = ({ onToggleCartHidden, itemCount }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
-  const initialValue = 0;
-  return {
-    itemCount: cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity,
-      initialValue
-    ),
-  };
-};
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   onToggleCartHidden: () => dispatch(toggleCartHidden()),

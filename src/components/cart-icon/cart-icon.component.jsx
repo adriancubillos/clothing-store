9496f6a6a337d58a86d53915700da9e89ce0bd09
Adrian <adriancubillos@gmail.com>
2022-03-10 11:50:00 -0500

    Adding Cart reducer and connecting it to cart-icon compoennt

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-  <div className='cart-icon'>
+const CartIcon = ({ onToggleCartHidden }) => (
+  <div className='cart-icon' onClick={onToggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
     <span className='item-count'>0</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = (dispatch) => ({
+  onToggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);

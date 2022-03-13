944baa4d280dddea3e70422dc54f37ff0e0b9b0d
Adrian <adriancubillos@gmail.com>
2022-03-13 01:06:51 -0500

    Adding shop slice to state in reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './shop.data';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-export default class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ collections }) => {
+  return (
+    <div className='shop-page'>
+      {collections.map(({ id, items, title }) => (
+        <CollectionPreview key={id} items={items} title={title} />
+      ))}
+    </div>
+  );
+};
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, items, title }) => (
-          <CollectionPreview key={id} items={items} title={title} />
-        ))}
-      </div>
-    );
-  }
-}
+export default connect(mapStateToProps)(ShopPage);

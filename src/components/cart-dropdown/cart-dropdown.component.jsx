19ec4114f4acb5875a42177198150cfecd73807a
Adrian <adriancubillos@gmail.com>
2022-03-12 19:45:01 -0500

    Empty shopping cart image and message

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,15 +4,23 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { ReactComponent as EmptyShoppingCart } from '../../assets/empty-cart.svg';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropDown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <div className='empty-cart'>
+          <EmptyShoppingCart className='empty-cart-image' />
+          <span className='empty-message'>Your cart is empty!</span>
+        </div>
+      )}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>

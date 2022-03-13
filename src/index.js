7ed2c1aa0de1b05083bfdb02b2ffb10baddd42c2
Adrian <adriancubillos@gmail.com>
2022-03-13 00:03:55 -0500

    Adding local storage capabilities for redux to persist slices of the store

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import './index.css';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Router>
     </Provider>
   </React.StrictMode>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
-import cartReducer from './cart/cart.reducer';
 
+import persistReducer from 'redux-persist/lib/persistReducer';
+import storage from 'redux-persist/lib/storage';
+
+import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'], //Store slices we want to persist
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
